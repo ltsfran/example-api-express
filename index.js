b772778ce9ca82d5e6e22718eba6768d38503f8b
Luis Tupa <luis.tupa@neoauto.pe>
2023-01-31 21:38:26 -0500

    feat: add users api

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const product = require('./api/product')
+const users = require('./api/users')
 
 app.use(express.json({ extended: false }))
 
 app.use('/api/product', product)
+app.use('/api/users', users)
 
 const PORT = 3000
 app.listen(PORT, () => console.log('Listen on PORT:', 3000))
